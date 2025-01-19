607f096d3f1016ba744b91515be7ec3a19148b2d
lintin0723 <lintin0723@gmail.com>
2025-01-20 00:30:11 +0800

    商品功能 - 實作修改商品

--- src/main/java/com/lintin0723/springbootmall/controller/ProductController.java
+++ src/main/java/com/lintin0723/springbootmall/controller/ProductController.java
@@ -33,4 +33,23 @@ public class ProductController {
 
         return ResponseEntity.status(HttpStatus.CREATED).body(product);
     }
+
+    @PutMapping("/products/{productId}")
+    public ResponseEntity<Product> updateProduct(@PathVariable Integer productId,
+                                                 @RequestBody @Valid ProductRequest productRequest){
+        Product product = productService.getProductById(productId);
+
+        //檢查product是否存在
+        if (product == null){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
+        //修改商品的數據
+        productService.updateProduct(productId, productRequest);
+
+        Product updateProduct = productService.getProductById(productId);
+
+        return ResponseEntity.status(HttpStatus.OK).body(updateProduct);
+    }
+
 }
