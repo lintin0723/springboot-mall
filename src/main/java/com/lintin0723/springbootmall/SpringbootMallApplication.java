7c9e5c58739f2b6fae10a18a623aa1b4a363906d
lintin0723 <lintin0723@gmail.com>
2025-01-19 10:58:36 +0800

    初始化Spring Boot project

---
+++ src/main/java/com/lintin0723/springbootmall/SpringbootMallApplication.java
@@ -0,0 +1,13 @@
+package com.lintin0723.springbootmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lintin0723/springbootmall/SpringbootMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lintin0723.springbootmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
