005a8d3297a9df772c700376e1bf7c6672900d82
lintin0723 <lintin0723@gmail.com>
2025-01-19 18:36:35 +0800

    商品功能 - 實作新增商品

--- src/main/java/com/lintin0723/springbootmall/dao/ProductDao.java
+++ src/main/java/com/lintin0723/springbootmall/dao/ProductDao.java
@@ -1,8 +1,11 @@
 package com.lintin0723.springbootmall.dao;
 
+import com.lintin0723.springbootmall.dto.ProductRequest;
 import com.lintin0723.springbootmall.model.Product;
 
 public interface ProductDao {
 
     Product getProductById(Integer productId);
+
+    Integer createProduct(ProductRequest productRequest);
 }
