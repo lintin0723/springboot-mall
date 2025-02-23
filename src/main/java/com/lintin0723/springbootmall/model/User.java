7de28d40f5d51224d449c5d83a6c86b62bc1ac04
lintin0723 <lintin0723@gmail.com>
2025-02-23 11:35:55 +0800

    帳號功能 - 檢查註冊的email，隱藏返回的密碼

--- src/main/java/com/lintin0723/springbootmall/model/User.java
+++ src/main/java/com/lintin0723/springbootmall/model/User.java
@@ -1,11 +1,15 @@
 package com.lintin0723.springbootmall.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import java.util.Date;
 
 public class User {
 
     private Integer userId;
     private String email;
+
+    @JsonIgnore
     private String password;
     private Date createDate;
     private Date lastModifiedDate;
--- src/main/java/com/lintin0723/springbootmall/service/impl/UserServiceImpl.java
+++ src/main/java/com/lintin0723/springbootmall/service/impl/UserServiceImpl.java
@@ -4,22 +4,36 @@ import com.lintin0723.springbootmall.dao.UserDao;
 import com.lintin0723.springbootmall.dto.UserRegisterRequest;
 import com.lintin0723.springbootmall.model.User;
 import com.lintin0723.springbootmall.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
 
 @Component
 public class UserServiceImpl implements UserService {
 
+    private final static Logger log = LoggerFactory.getLogger(UserServiceImpl.class);
     @Autowired
     private UserDao userDao;
-
-    @Override
-    public Integer register(UserRegisterRequest userRegisterRequest) {
-        return userDao.createUser(userRegisterRequest);
-    }
 
     @Override
     public User getUserById(Integer userId) {
         return userDao.getUserById(userId);
     }
+
+    @Override
+    public Integer register(UserRegisterRequest userRegisterRequest) {
+        // 檢查註冊email
+        User user = userDao.getUserByEmail(userRegisterRequest.getEmail());
+
+        if(user != null){
+            log.warn("This email {} has been register", userRegisterRequest.getEmail());
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+        //建立帳號
+        return userDao.createUser(userRegisterRequest);
+    }
+
 }
